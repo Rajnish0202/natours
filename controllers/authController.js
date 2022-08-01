fafce4ea24e8d82dd2d1e231959091d21bc4ea94
Rajnish0202 <rajnish.0202kumar@gmail.com>
2022-08-01 13:51:18 +0530

    Added heroku config

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,19 +12,21 @@ const signToken = (id) => {
   });
 };
 
-const createSendToken = (user, statusCode, res) => {
+const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
 
-  const cookiesOptions = {
+  // if (process.env.NODE_ENV === 'production') cookiesOptions.secure = true;
+  // if (req.secure || req.headers('x-forwarded--proto') === 'https')
+  //   cookiesOptions.secure = true;
+
+  res.cookie('jwt', token, {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     // secure: true,
     httpOnly: true,
-  };
-
-  if (process.env.NODE_ENV === 'production') cookiesOptions.secure = true;
-  res.cookie('jwt', token, cookiesOptions);
+    secure: req.secure || req.headers('x-forwarded--proto') === 'https',
+  });
 
   user.password = undefined;
 
@@ -50,7 +52,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   const url = `${req.protocol}://${req.get('host')}/me`;
   // console.log(url);
   await new Email(newUser, url).sendWelcome();
-  createSendToken(newUser, 201, res);
+  createSendToken(newUser, 201, req, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -70,7 +72,7 @@ exports.login = catchAsync(async (req, res, next) => {
   }
 
   // 3) If everything ok, send token to client
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
 });
 
 exports.logout = (req, res) => {
@@ -239,7 +241,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   // 3) Update changedPasswordAt property for the user
   // 4) Log the  user in. send JWT
 
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
 });
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
@@ -261,5 +263,5 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
 
   // 4) Log the user in, send JWT
 
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
 });
